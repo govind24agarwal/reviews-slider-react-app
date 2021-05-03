008a89026afb9100731645e7411ee7b94cc76318
Govind <govindagarwal24@gmail.com>
2021-05-04 03:25:44 +0530

    styling done, quotes visible on the scree, slide functionality not added.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import { FaQuoteLeft } from "react-icons/fa";
+import { GrLinkNext, GrLinkPrevious } from "react-icons/gr";
+import data from "./data";
 function App() {
-  return <div>Slider</div>;
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="section-center">
+        <div className="title">
+          <h2>Reviews</h2>
+        </div>
+        {people.map((person) => {
+          const { id, name, image, title, quote } = person;
+          return (
+            <article key={id} className="person">
+              <FaQuoteLeft className="quote-mark" />
+              <div className="person-content">
+                <p className="quote">{quote}</p>
+                <div className="person-footer">
+                  <img src={image} alt={name} />
+                  <div className="person-detail">
+                    <h3>{name}</h3>
+                    <h4>{title}</h4>
+                  </div>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+        <button className="prev">
+          <GrLinkPrevious />
+        </button>
+        <button className="next">
+          <GrLinkNext />
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
